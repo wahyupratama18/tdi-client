c1101e5285191b1f1d16ba87b8d6f205bf7e3d48
Wahyu Styo Pratama <42894387+wahyupratama18@users.noreply.github.com>
2023-08-12 23:32:32 +0700

    Modify dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                Selamat datang di menu administrasi TDI
+                <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+                    <h1 class="text-2xl font-medium text-gray-900">
+                        Selamat datang di menu administrasi TDI
+                    </h1>
+                </div>
                 {{-- <x-welcome /> --}}
             </div>
         </div>
